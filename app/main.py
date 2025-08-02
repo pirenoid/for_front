6c2a4d5b58fa6005b6c0c9321105459bef4d9856
pirenoid <irpetrykina@yandex.ru>
2025-08-02 13:13:57 +0300

    add cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, Query, HTTPException
 from typing import Optional, List
 from enum import Enum
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
+                   "Access-Control-Allow-Origin", "Authorization"],
+)
 
 persons_db = {
     "1": {
